9027350b9720e3e2ea7bd57d9ee8a738dd94f1a3
BernieJ <berniej67@gmail.com>
2021-08-23 05:38:53 +0200

    validate type of client
    Fix validation of type of client

--- Sample.D365.Solution/WebResources/new_/Script/Core/contact.js
+++ Sample.D365.Solution/WebResources/new_/Script/Core/contact.js
@@ -1,63 +1,77 @@
-function disableNameInput() {
-    Xrm.Page.getControl('Individual Client').setDisabled(true);
-    Xrm.Page.getControl('First Name').setDisabled(true);
-    Xrm.Page.getControl('Last Name').setDisabled(true);
-    Xrm.Page.getControl('Corporate Client Name').setDisabled(true);
+function disableNameInput(formContext) {
+
+    //formContext.getControl('fullname').setDisabled(true);
+    formContext.getControl('fullname_compositionLinkControl_firstname').setDisabled(true);
+    formContext.getControl('fullname_compositionLinkControl_middlename').setDisabled(true);
+    formContext.getControl('fullname_compositionLinkControl_lastname').setDisabled(true);
+    formContext.getControl('cr53a_corporateclientname').setDisabled(true);
 }
 
-function validateTypeOfClient() {
-    disableNameInput();
+function validateTypeOfClient(executionContext) {
+    var formContext = executionContext.getFormContext();
 
+    disableNameInput(formContext);
+
+    let prefferedContactMethodCode = formContext.getControl('cr53a_typeofclient').getAttribute().getValue();
     switch (prefferedContactMethodCode) {
         case 339520000: // Type = Corporate Client
             {
-                hideIndividualClientDetails();
-                enableCorporateClientNameInput();
+                hideIndividualClientDetails(formContext);
+                enableCorporateClientNameInput(formContext);
                 break;
             }
         case 339520001: // Type = Individual Client
             {
-                hideCorporateClientDetails();
-                enableIndividualClientInput();
+                hideCorporateClientDetails(formContext);
+                enableIndividualClientInput(formContext);
                 break;
             }
     }
 }
 
-function enableCorporateClientNameInput() {
-    Xrm.Page.getControl('Corporate Client Name').setDisabled(false);
-}
-function enableIndividualClientInput() {
-    Xrm.Page.getControl('Individual Client').setDisabled(false);
-    Xrm.Page.getControl('First Name').setDisabled(false);
-    Xrm.Page.getControl('Last Name').setDisabled(false);
+function enableCorporateClientNameInput(formContext) {
+    formContext.getControl('cr53a_corporateclientname').setDisabled(false);
+    formContext.getControl('cr53a_corporateclientname').setVisible(true);
 }
 
-function hideCorporateClientDetails() {
-    Xrm.Page.getControl('Corporate Client Name').setVisible(false);
+function enableIndividualClientInput(formContext) {
+    formContext.getControl('fullname_compositionLinkControl_firstname').setDisabled(false);
+    formContext.getControl('fullname_compositionLinkControl_middlename').setDisabled(false);
+    formContext.getControl('fullname_compositionLinkControl_lastname').setDisabled(false);
+
+    formContext.getControl('fullname_compositionLinkControl_firstname').setVisible(true);
+    formContext.getControl('fullname_compositionLinkControl_middlename').setVisible(true);
+    formContext.getControl('fullname_compositionLinkControl_lastname').setVisible(true);
 }
 
-function hideIndividualClientDetails() {
-    Xrm.Page.getControl('Individual Client').setVisible(false);
-    Xrm.Page.getControl('First Name').setVisible(false);
-    Xrm.Page.getControl('Last Name').setVisible(false);
+function hideCorporateClientDetails(formContext) {
+    formContext.getControl('cr53a_corporateclientname').setVisible(false);
+}
+
+function hideIndividualClientDetails(formContext) {
+    formContext.getControl('fullname_compositionLinkControl_firstname').setVisible(false);
+    formContext.getControl('fullname_compositionLinkControl_middlename').setVisible(false);
+    formContext.getControl('fullname_compositionLinkControl_lastname').setVisible(false);
 }
 
 function validatePreferredMethodOfCommunication() {
-    switch (prefferedContactMethodCode) {
+
+    let PreferredMethodOfCommunication = Xrm.Page.getControl('PreferredContactMethodCode').getValue();
+
+    switch (PreferredMethodOfCommunication) {
         case 1: // Preferred Method = Any
             clearAllMandatoryFields();
             break;
         case 2: // Preferred Method = Email
             {
                 clearAllMandatoryFields();
-                Xrm.Page.getAttribute('Email').setRequiredLevel('required');
+                Xrm.Page.getAttribute('email').setRequiredLevel('required');
                 break;
             }
         case 3: //Preferred Method = Phone
             {
                 clearAllMandatoryFields();
-                Xrm.Page.getAttribute('Mobile Phone').setRequiredLevel('required');
+                Xrm.Page.getAttribute('mobilephone').setRequiredLevel('required');
                 break;
             }
     }
@@ -65,8 +79,8 @@ function validatePreferredMethodOfCommunication() {
 
 function clearAllMandatoryFields() {
 
-    Xrm.Page.getAttribute('Email').setRequiredLevel('none');
-    Xrm.Page.getAttribute('Mobile Phone').setRequiredLevel('none');
+    Xrm.Page.getAttribute('email').setRequiredLevel('none');
+    Xrm.Page.getAttribute('mobilephone').setRequiredLevel('none');
 }
 
 function ExtentInvestment(numberOfMonths) {
@@ -93,13 +107,13 @@ function SetMatured() {
         lookupVal[0].name = Xrm.Page.data.entity.firstname;
 
         lookupVal[0].entityType = "contact";
-        Xrm.Page.getAttribute('Status Reason').setValue('Matured');
+        Xrm.Page.getAttribute('StatusCode').setValue('Matured');
 
         Xrm.Page.data.entity.save();
 
         Xrm.Page.data.refresh();
     }
     else {
-        Xrm.Page.ui.setFormNotification("Investment havn't matured yet. Maturity date is" + Xrm.Page.data.entity.InvestmentPeriodMonths.toString(), ...);
+        Xrm.Page.ui.setFormNotification("Investment havn't matured yet. Maturity date is" + Xrm.Page.data.entity.InvestmentPeriodMonths.toString());
     }
 }
